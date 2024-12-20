b13c51dfb1c5354cdf3d1de48739776b413adb75
khatun1112 <khatavet2004@gmail.com>
2024-12-20 20:15:44 +0400

    Portfolios

---
+++ Mery/src/components/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Data Scientist</h3>
+              <p>
+                I'm a data scientist with experience in data analysis, data visualization, and creating machine learning models.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Machine Learning Developer</h3>
+              <p>
+                I specialize in developing machine learning algorithms, optimizing models, and deploying them to production.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>AI/ML Researcher</h3>
+              <p>
+                I focus on cutting-edge research in artificial intelligence and machine learning to develop innovative solutions.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
